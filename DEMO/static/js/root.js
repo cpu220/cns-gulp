3016e203eea945a467279810b33357e2d8978508
Belial <102864814@qq.com>
2015-06-15 17:21:16 +0800

    add common style

--- DEMO/static/js/root.js
+++ DEMO/static/js/root.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 	var root = {
 		init: function(fileName) {
 			this.doAjax(fileName);
-			
+
 		},
 		/* 获取本地动态生成的ip地址，用于手机端即时访问
-		* return {object Object} 将ip.json获取后直接用于使用
-		*/
+		 * return {object Object} 将ip.json获取后直接用于使用
+		 */
 		doAjax: function(fileName) {
 			var _this = this;
 			jQuery.ajax({
 				type: "GET",
-				url: fileName+".json",
+				url: fileName + ".json",
 				dataType: "json",
 				success: function(data) {
 					a = data;
@@ -23,38 +23,43 @@ $(document).ready(function() {
 			});
 		},
 		/* 创建跳转地址的DOM
-		* {object Object} 跳转地址的对应参数
-		*/
+		 * {object Object} 跳转地址的对应参数
+		 */
 		creatDOM: function(json) {
 			var _this = this,
-				strURL = "",
-				setQrCode="";
-			for (var i in json.ip) {
-				var num=(parseInt(i) + 1),
-					url= "http://"+json.ip[i] + ":" + json.port + "/"+json.index;
-				strURL += "<td><a data-id='a"+num+"' href='"+url+"' >地址" + num + " [" + json.ip[i] + " ]</a></td>";
-				setQrCode+="<td><div id='a"+num+"' data-url='"+url+"'></div></td>"; 
-			}
-			$("#qrcode").append(setQrCode);
-			$("#address").append(strURL);
+				str = "";
 
-			for(var i=0;i<$("#qrcode td").length;i++){
-				_this.setQrCode({
-					id:$("#qrcode div").eq(i)[0],
-					url:$("#qrcode div").eq(i).data("url")
-				});
+			for (var i in json.ip) {
+				var num = (parseInt(i) + 1),
+					url = "http://" + json.ip[i] + ":" + json.port + "/" + json.index;
+					console.log(str);
+				str += "<li data-num='" + num + "'>" +
+					"<div class='qrcodeBox'  >" +
+					"<div id='qrcode" + num + "' class='qrcodeIMG' data-url='"+url+"' ></div>" +
+					"<div class='qrcode-title'><a href='" + url + "'>地址" + num + ":[" + json.ip[i] + "]</a></div>"+ 
+					"</div></li>"; 
+
 			}
+			$("#qrcodeList").append(str);
+
+			$(".qrcodeIMG").each(function(){
+				_this.setQrCode({
+					id: $(this)[0],
+					url: $(this).data("url")
+				});
+			})
+			 
 		},
 		/* 生成二维码
-		* {object Object} 二维码对象的信息
-		*/
+		 * {object Object} 二维码对象的信息
+		 */
 		setQrCode: function(object) {
-			 
+
 			var qrcode = new QRCode(object.id, {
 				width: 150, //设置宽高
 				height: 150
 			});
-			qrcode.makeCode(object.url); 
+			qrcode.makeCode(object.url);
 		}
 	};
 	root.init("ip");
