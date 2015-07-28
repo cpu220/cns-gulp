9106f2c622cce54f613bb6a1076b21351d8275bf
Belial <102864814@qq.com>
2015-07-28 14:38:26 +0800

    123

--- service.js
+++ service.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var net = require('net');
+
 var url = require('url');
 var fs = require('fs');
 var util = require('util');
@@ -7,40 +9,40 @@ var fileType = require('./fileType').types;
 var path = require('path');
 var child_process = require("child_process");
 var os = require('os');
-  
+
 
 
 var CNServer = {
-	opations:JSON.parse(fs.readFileSync('config.json')),
+	opations: JSON.parse(fs.readFileSync('config.json')),
+
 	init: function() {
-		var _this=this;
-		var port=_this.opations.port || 8080;
+		var _this = this;
+		var port = _this.opations.port || 8080;
 		this.creatService(port);
 		this.opations.autoOpenBrowser ? (this.openWindow(port)) : "";
 
 	},
 	getHostIP: function() {
-		var IPv4, hostName;
-		hostName = os.hostname();
+		var hostName = os.hostname();
 		var ifaces = os.networkInterfaces();
 		var ip = [];
 		for (var x in ifaces) {
 
 			for (var y in ifaces[x]) {
 				var object = ifaces[x][y];
-				if (object["family"] === this.opations.ipType) {
-					ip.push(object.address); 
+				if (object["family"] === this.opations.service.ipType) {
+					ip.push(object.address);
 				}
 			}
 
 		}
 		var json = {
 			ip: ip,
-			host: hostName,
-			system:os.type(),
-			release:os.release(),
-			port: this.opations.port,
-			index: this.opations.indexHTML
+			host: this.opations.service.hostName ? hostName : "",
+			system: os.type(),
+			release: os.release(),
+			port: this.opations.service.port,
+			index: this.opations.html.indexHTML
 		};
 
 		fs.writeFile("./DEMO/ip.json", JSON.stringify(json), function(err) {
@@ -52,7 +54,7 @@ var CNServer = {
 	},
 	openWindow: function(port) {
 		var _this = this;
-
+		var html = this.opations.html;
 		if (process.platform == 'win32') {
 
 			cmd = 'start "%ProgramFiles%\Internet Explorer\iexplore.exe"';
@@ -68,17 +70,17 @@ var CNServer = {
 		}
 		var pc = _this.getHostIP();
 
-		var location = "http://" + pc.ip[0] + ":" + port + "/" + (this.opations.index ? this.opations.indexHTML : this.opations.rootHTML);
+		var location = "http://" + pc.ip[0] + ":" + port + "/" + (html.index ? html.indexHTML : html.rootHTML);
 		child_process.exec(cmd + ' "' + location + '"');
 
-		user = "----- [" + pc.host + "]["+ pc.system+"]["+pc.release+"] [" + _this.formatDate(new Date()) + "]访问 ----\n";
+		var user = "----- [" + pc.host + "][" + pc.system + "][" + pc.release + "] [" + _this.formatDate(new Date()) + "] ----\n";
 
 		_this.appendLog(user);
 
 	},
 	getPath: function(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		var realPath = path.join(this.opations.root, pathname);
+		var realPath = path.join(this.opations.html.root, pathname);
 
 		var ext = path.extname(realPath);
 		ext = ext ? ext.slice(1) : 'unknown';
@@ -91,7 +93,7 @@ var CNServer = {
 	},
 	resetLog: function() {
 		var _this = this;
-		if (_this.opations.resetLog) {
+		if (_this.opations.log.resetLog) {
 			fs.writeFile("log.txt", "", function(err) {
 				if (err) {
 					throw err;
@@ -119,32 +121,36 @@ var CNServer = {
 	},
 	//读取资源
 	setLoadResources: function(request, response) {
+
 		var _this = this,
 			path = _this.getPath(request, response);
-		var message = path.realPath + "---------- [" + ( _this.formatDate(new Date() ) ) + "]  \n";
+		// var pc = _this.getHostIP();
+		 
+		 
+		var message = "["+os.platform()+"] "+ path.realPath + "---------- [" + (_this.formatDate(new Date())) + "]  \n";
 		return message;
 	},
 	//时间格式format
-	formatDate:function(date){
-		var arr=["00","01","02","03","04","05","06","07","08","09"];
-		var D=date.getDate() ,
-			M=date.getMonth()+1,
-			Y=date.getFullYear(),
-			h=date.getHours(),
-			m=date.getMinutes(),
-			s=date.getSeconds();
-		 
-		return   Y+"-"+ (arr[M]||M) + "-"+(arr[D]||D)+" "+
-				 (arr[h]||h)+":"+(arr[m]||m)+":"+(arr[s]||s);
+	formatDate: function(date) {
+		var arr = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"];
+		var D = date.getDate(),
+			M = date.getMonth() + 1,
+			Y = date.getFullYear(),
+			h = date.getHours(),
+			m = date.getMinutes(),
+			s = date.getSeconds();
+
+		return Y + "-" + (arr[M] || M) + "-" + (arr[D] || D) + " " +
+			(arr[h] || h) + ":" + (arr[m] || m) + ":" + (arr[s] || s);
 	},
 	page404: function(req, res, path) {
 		res.writeHead(404, {
 			'Content-Type': 'text/plain'
 		});
-		var error=http.STATUS_CODES[404]
+		var error = http.STATUS_CODES[404]
 		res.write("<!doctype html> \n");
-		res.write("<title>404 "+error+"</title> \n");
-		res.write("<h1>"+error+"</h1> \n");
+		res.write("<title>404 " + error + "</title> \n");
+		res.write("<h1>" + error + "</h1> \n");
 		res.write("<p>not found " + path + "</p>");
 		res.end();
 
@@ -153,19 +159,19 @@ var CNServer = {
 		res.writeHead(500, {
 			'Content-Type': 'text/plain'
 		});
-		var error=http.STATUS_CODES[500];
+		var error = http.STATUS_CODES[500];
 		res.write("<!doctype html> \n");
-		res.write("<title>500 "+error+"</title> \n");
-		res.write("<h1>"+error+"</h1> \n");
+		res.write("<title>500 " + error + "</title> \n");
+		res.write("<h1>" + error + "</h1> \n");
 		res.write("<p>" + util.inspect(err) + "</p>");
 		res.end();
 	},
 	creatService: function(port) {
 		var _this = this;
 		_this.resetLog();
-		 
 
-		var server = http.createServer(function(req, res) {
+
+		_this.server = http.createServer(function(req, res) {
 
 			var path = _this.getPath(req, res);
 			fs.exists(path.realPath, function(exists) {
@@ -184,19 +190,28 @@ var CNServer = {
 							});
 							res.write(file, "binary");
 							res.end();
-							_this.opations.resourcesLog ? (_this.setLog(req, res)) : "";
+							_this.opations.log.resourcesLog ? (_this.setLog(req, res)) : "";
 
 						}
 					});
 
 				}
 			});
-			 
 
-		}).listen(port, function() {
+
+		});
+		_this.server.listen(port, function() {
 			console.log("目前服务器版本为v1.0,如有问题请自行解决");
 			console.log("Server runing at port: " + port);
-		}) ;
+
+		});
+		_this.server.on("connection",function(socket){
+			 // console.log(socket.remoteAddress );
+			 // console.log(socket.address().address);
+			 // _this.appendLog(message);
+			 _this.appendLog("["+socket.remoteAddress+"] \n");
+		});
+ 
 
 
 	}
